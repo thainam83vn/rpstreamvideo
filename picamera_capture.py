5af9772af96143a29339c5349d8b82b5b66ad186
thai <thainam83vn@gmail.com>
2017-05-27 10:39:10 -0400

    u

--- picamera_capture.py
+++ picamera_capture.py
@@ -4,11 +4,12 @@ from datetime import datetime
 
 # Explicitly open a new file called my_image.jpg
 
+i = 0
 with picamera.PiCamera() as camera:
     camera.start_preview()
     while True:
         time.sleep(0.1)
-        file_name = "img" + datetime.time()
+        file_name = "" + i + ".jpg"
         my_file = open("images/" + file_name, 'wb')
         camera.capture(my_file)
         my_file.close()
