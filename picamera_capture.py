680623f542f2fb7cba89a128eac75c8b86a1e355
thai <thainam83vn@gmail.com>
2017-05-27 10:35:00 -0400

    u

--- picamera_capture.py
+++ picamera_capture.py
@@ -1,12 +1,17 @@
 import time
 import picamera
+from datetime import datetime
 
 # Explicitly open a new file called my_image.jpg
-my_file = open('my_image.jpg', 'wb')
+
 with picamera.PiCamera() as camera:
     camera.start_preview()
-    time.sleep(2)
-    camera.capture(my_file)
+    while True:
+        time.sleep(0.1)
+        file_name = "img" + datetime.time()
+        my_file = open("images/" + file_name, 'wb')
+        camera.capture(my_file)
+        my_file.close()
 # At this point my_file.flush() has been called, but the file has
 # not yet been closed
-my_file.close()
+
